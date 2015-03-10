88a030ded01df604b6f83544062bc9fe4699eed5
chen <chen@chen-LIFEBOOK-AH532-G52>
2015-03-10 13:13:21 +0100

    Session handling

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@ require_once 'Facebook/GraphObject.php';
 require_once 'Facebook/Entities/AccessToken.php';
 require_once 'Facebook/HttpClients/FacebookCurlHttpClient.php';
 require_once 'Facebook/HttpClients/FacebookHttpable.php';
-
+require_once 'Facebook/GraphSessionInfo.php';
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
@@ -36,14 +36,17 @@ use Facebook\GraphObject;
 use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
-	
+use Facebook\GraphSessionInfo;
 
+
+$appid = 'xxx'; // your AppID
+$secret = 'xxx'; // your secret
 
 
 // unset($_SESSION['access_token']);
 
 // init app with app id (APPID) and secret (SECRET)
-FacebookSession::setDefaultApplication('xxx', 'xxx');
+FacebookSession::setDefaultApplication($appid ,$secret);
 	
 // login helper with redirect_uri
 $helper = new FacebookRedirectLoginHelper ( 'http://BFAPI.com/');
@@ -57,14 +60,47 @@ try {
 	$session = $helper->getSessionFromRedirect ();
 	// var_dump ( $session );
 } catch ( FacebookRequestException $ex ) { // When Facebook returns an error
-	echo "Error from FB" . $ex;
+	echo "Error from FB <br>" . $ex;
 } catch ( \Exception $ex ) { // When validation fails or other local issues
-	echo "Something's wrong with the validation" . $ex;
+	echo "Something's wrong with the validation <br>" . $ex;
 }
+
+// see if we have a session in $_SESSION[]
+if( isset($_SESSION['token']))
+{
+	// We have a token, is it valid?
+	$session = new FacebookSession($_SESSION['token']);
+	try
+	{
+		$session->Validate($appid ,$secret);
+	}
+	catch( FacebookAuthorizationException $ex)
+	{
+		// Session is not valid any more, get a new one.
+		$session ='';
+	}
+}
+
 
  //see if we have a session
 if ($session) { // Logged in
-	echo "<b>-Graph API-</b><br><br>";
+	
+	echo "<b>-Session Info-</b><br><br>";
+	// set the PHP Session 'token' to the current session token
+	$_SESSION['token'] = $session->getToken();
+	// SessionInfo
+	$info = $session->getSessionInfo();
+	var_dump($info);
+	// getAppId
+	echo "Appid: " . $info->getAppId() . "<br />";
+	// session expire data
+	$expireDate = $info->getExpiresAt()->format('Y-m-d H:i:s');
+	echo 'Session expire time: ' . $expireDate . "<br />";
+	// session token
+	echo 'Session Token: ' . $session->getToken() . "<br />";
+	
+	
+	echo "<br /><b>-Graph API-</b><br><br>";
 	// graph api request for user data
 	$request = new FacebookRequest($session, 'GET', '/me');
 	$response = $request->execute();
@@ -73,10 +109,30 @@ if ($session) { // Logged in
 	// print data
 	//echo  print_r( $graphObject, 1 );
 	var_dump($graphObject);
+	
+	
+	displayCustomFBPageFeed($session);
+	
+	
+	
 } else {
   // show login url
   echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
 }
+
+// from callmenick.com/2013/03/14/displaying-a-custom-facebook-page-feed/
+function displayCustomFBPageFeed($session){
+	// first get pageid
+	
+	
+	
+	$pageid = '100001456337135';
+	//$request = new FacebookRequest($session, 'GET', '/{'.$pageid.'}/feed');
+	$request = new FacebookRequest($session, 'GET', '/100001456337135/feed');
+	$response = $request->execute();
+	var_dump($response);
+}
+
 
 
 ?>
